470b425d09d5a48d3e310b90f3bccbb5197131eb
Manav Aggarwal <83641534+manavagr1108@users.noreply.github.com>
2024-01-22 02:06:06 +0530

    Revert "Add cursor tool to ToolBar"

--- src/classes/DrawChart.js
+++ src/classes/DrawChart.js
@@ -40,9 +40,6 @@ class DrawChart{
     this.drawChartFunction = drawChart;
     this.isIndicator = signal(false);
     this.Indicator = signal(null);
-
-    // trend lines data;
-    this.trendLinesData = signal([]);
   }
   #setData(data) {
     this.data.value = data;
