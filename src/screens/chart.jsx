9ff0a7bc6f3abedbdb7049237d5d0b1f28a1f608
Shlok Upadhyay <deep.upu9@gmail.com>
2024-02-02 18:09:34 +0530

    Feat: Added Routing

---
+++ src/screens/chart.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Charting from "../components/charting/charting";
+import NavBar from "../components/nav_bar/nav_bar";
+import StockSelect from "../components/stock_select/stock_select";
+import ToolBar from "../components/tool_bar/tool_bar";
+import useChartWindow from "../hooks/useChartWindow";
+
+function Chart() {
+  const [mode, setMode] = useState("Light");
+  function toggleMode() {
+    setMode((prev) => (prev === "Light" ? "Dark" : "Light"));
+  }
+  const ChartWindow = useChartWindow(mode);
+  return (
+    <div className="app">
+      <div className="grid grid-cols-[3rem_1fr] grid-rows-[3rem_1fr] h-screen">
+        <StockSelect mode={mode} />
+        <NavBar mode={mode} ChartWindow={ChartWindow} toggleMode={toggleMode} />
+        <ToolBar mode={mode} ChartWindow={ChartWindow} />
+        <Charting mode={mode} ChartWindow={ChartWindow} />
+      </div>
+    </div>
+  );
+}
+
+export default Chart;
