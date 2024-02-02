ef9e8d314813f6468f9d0480a50329ea722813dc
Shlok Upadhyay <deep.upu9@gmail.com>
2024-02-03 00:51:36 +0530

    Feat: Login and Register Integration

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,9 +1,76 @@
-function Login() {
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}login`,
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const responseData = response.data;
+      console.log("Login successful:", responseData);
+
+      toast.success("Login successful!");
+      navigate("/");
+    } catch (error) {
+      toast.error(error.response?.data.message || "Login failed");
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="max-w-md mx-auto my-10 p-6 bg-white rounded-md shadow-md">
+      <form>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Username:
+          </label>
+          <input
+            className="w-full border rounded-md p-2"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password:
+          </label>
+          <input
+            className="w-full border rounded-md p-2"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          type="button"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </form>
+      {error && <p className="text-red-500 mt-4">{error}</p>}
     </div>
   );
-}
+};
 
 export default Login;
