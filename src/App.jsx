928980326b0987c3c939f4e70885f2a053bca6cd
Shlok Upadhyay <deep.upu9@gmail.com>
2024-02-02 18:09:34 +0530

    Feat: Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
 import "./App.css";
-import Charting from "./components/charting/charting";
-import NavBar from "./components/nav_bar/nav_bar";
-import StockSelect from "./components/stock_select/stock_select";
-import ToolBar from "./components/tool_bar/tool_bar";
-import useChartWindow from "./hooks/useChartWindow";
+import Chart from "./screens/chart";
+import Login from "./screens/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("Light");
-  function toggleMode() {
-    setMode((prev) => (prev === "Light" ? "Dark" : "Light"));
-  }
-  const ChartWindow = useChartWindow(mode);
   return (
-    <div className="app">
-      <div className="grid grid-cols-[3rem_1fr] grid-rows-[3rem_1fr] h-screen">
-        <StockSelect mode={mode} />
-        <NavBar mode={mode} ChartWindow={ChartWindow} toggleMode={toggleMode} />
-        <ToolBar mode={mode} ChartWindow={ChartWindow} />
-        <Charting mode={mode} ChartWindow={ChartWindow} />
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Chart />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
